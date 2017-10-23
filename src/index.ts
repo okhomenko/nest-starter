e28462a9a9d89b030b18c7ed3749e42da0a4f279
Oleksandr Khomenko <oleksandr_khomenko_cw@bmc.com>
2017-10-23 11:46:48 -0500

    Added interceptorsˆ

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import * as bodyParser from 'body-parser';
 import { ApplicationModule } from './modules/application.module';
+import { LoggingInterceptor } from './modules/common/interceptors/logging.interceptor';
 import { ValidationPipe } from './modules/common/pipes/validation.pipe';
 
 async function bootstrap() {
     const app = await NestFactory.create(ApplicationModule);
     app.use(bodyParser.json());
     app.useGlobalPipes(new ValidationPipe());
+    app.useGlobalInterceptors(new LoggingInterceptor());
     await app.listen(3000);
 }
 
